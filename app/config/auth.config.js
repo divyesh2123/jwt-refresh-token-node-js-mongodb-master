a36c86032e78ee23c57db07ffa16ebbae2686ebe
lenovo <lenovo@LAPTOP-BLNTEBH7>
2024-08-09 15:42:10 +0530

    d

---
+++ app/config/auth.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  secret: "#@$dsfsdfsadas",
+  // jwtExpiration: 3600,         // 1 hour
+  // jwtRefreshExpiration: 86400, // 24 hours
+
+  /* for test */
+  jwtExpiration: 3600,          // 1 minute
+  jwtRefreshExpiration: 86400,  // 2 minutes
+};
